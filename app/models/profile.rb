836074413056b24ab2d5db0b2cca999ea09d95c1
Racemus <flasmo@gmail.com>
2015-11-08 23:58:34 +0300

    Added validations to User and Profile models

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,19 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  validates :gender, inclusion: { in: %w(male female) }
+  validate :names
+  validate :sue_male
+
+  def names
+  	if first_name.nil? && last_name.nil?
+  	  errors.add(:first_name, "There are should be some names")
+  	end
+  end
+
+  def sue_male
+  	if first_name == "Sue" && gender == "male"
+  	  errors.add(:gender, "Maybe Sue is shemale?")
+  	end
+  end
 end
