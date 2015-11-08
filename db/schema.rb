90fc1dfae632494283ba348e5f136a31db55b482
Racemus <flasmo@gmail.com>
2015-11-08 21:59:19 +0300

    Seeding db

--- db/schema.rb
+++ db/schema.rb
@@ -40,9 +40,12 @@ ActiveRecord::Schema.define(version: 20151108132055) do
   create_table "todo_lists", force: :cascade do |t|
     t.string   "list_name"
     t.date     "list_due_date"
+    t.integer  "user_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
+
+  add_index "todo_lists", ["user_id"], name: "index_todo_lists_on_user_id"
 
   create_table "todolists_users", force: :cascade do |t|
     t.integer "user_id"
